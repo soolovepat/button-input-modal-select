10534304785ec40b5052d8c5e9c38be6fd36c20d
gyurison7 <orangetree401@gmail.com>
2023-07-07 14:23:50 +0900

    Add select

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import * as S from "../styles/SelectStyle";
 
 const Select = () => {
   return (
-    <div>
-      <h1>Select</h1>
-    </div>
-  );
-};
+      <div>
+          <S.SelectLayout>
+              <h1>Select </h1>
+              <S.SelectComponents>
+                  <div>
+                      <S.SelectButton>
+                          <option>리액트</option>
+                          <option>자바</option>
+                          <option>스프링</option>
+                          <option>리액트네이티브</option>
+                      </S.SelectButton>
+                  </div>
+              </S.SelectComponents>
+          </S.SelectLayout>
+      </div>
+  )
+}
 
 export default Select;
---
+++ src/styles/SelectStyle.jsx
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+export const SelectLayout = styled.div`
+    border: 3px solid rgb(221, 221, 221);
+    height: 200px;
+    overflow: hidden;
+    position: relative;
+    margin-top: 50px;
+`
+
+export const SelectComponents = styled.div`
+    display: flex;
+    gap: 10px;
+`
+export const SelectButton = styled.select`
+    border: 1px solid rgb(221, 221, 221);
+    height: 40px;
+    width: 300px;
+    background-color: rgb(255, 255, 255);
+    border-radius: 12px;
+
+`
