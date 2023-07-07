785bb19b4aa1147eb02b5bd4c0e0e6fbd6994db2
Sujin Lee <soolovepat@naver.com>
2023-07-07 21:25:10 +0900

    Fix button style 중복 제거

--- src/components/modal/ModalWindow.jsx
+++ src/components/modal/ModalWindow.jsx
@@ -1,22 +1,27 @@
+import { StButton } from "../../styles/ButtonStyle";
 import * as S from "../../styles/ModalWindowStyle";
 
-const ModalWindow = ({openModalHandler}) => {
-    return (
+const ModalWindow = ({ openModalHandler }) => {
+  return (
+    <div>
+      <S.ModalDiv></S.ModalDiv>
+      <S.ModalComponents>
         <div>
-            <S.ModalDiv></S.ModalDiv>
-            <S.ModalComponents>
-                <div>닫기와 확인 버튼 2개가 있고, 외부 영역을 눌러도 모달이 닫히지 않아요.</div>
-                <S.ButtonSet>
-                    <div>
-                        <S.CloseButton onClick={openModalHandler}>닫기</S.CloseButton>
-                    </div>
-                    <div>
-                        <S.CheckButton>확인</S.CheckButton>
-                    </div>
-                </S.ButtonSet>
-            </S.ModalComponents>
+          닫기와 확인 버튼 2개가 있고, 외부 영역을 눌러도 모달이 닫히지 않아요.
         </div>
-    )
-}
+        <S.ButtonSet>
+          <StButton backgroundColor="#fab1a0">
+            <button className="small-button" onClick={openModalHandler}>
+              닫기
+            </button>
+          </StButton>
+          <StButton backgroundColor="#55efc4">
+            <button className="small-button">확인</button>
+          </StButton>
+        </S.ButtonSet>
+      </S.ModalComponents>
+    </div>
+  );
+};
 
 export default ModalWindow;
--- src/styles/ModalWindowStyle.jsx
+++ src/styles/ModalWindowStyle.jsx
@@ -1,51 +1,31 @@
 import { styled } from "styled-components";
 
 export const ModalDiv = styled.div`
-    width: 100%;
-    height: 100vh;
-    inset: 0px;
-    position: fixed;
-    opacity: 0.8;
-    background-color: rgb(221, 221, 221);
-`
+  width: 100%;
+  height: 100vh;
+  inset: 0px;
+  position: fixed;
+  opacity: 0.8;
+  background-color: rgb(221, 221, 221);
+`;
 
 export const ModalComponents = styled.div`
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    border-radius: 12px;
-    box-sizing: border-box;
-    padding: 24px;
-    background-color: rgb(255, 255, 255);
-    width: 500px;
-    height: 300px;
-    position: absolute;
-`
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 12px;
+  box-sizing: border-box;
+  padding: 24px;
+  background-color: rgb(255, 255, 255);
+  width: 500px;
+  height: 300px;
+  position: absolute;
+`;
 
 export const ButtonSet = styled.div`
-    position: absolute;
-    bottom: 12px;
-    right: 12px;
-    display: flex;
-    gap: 5px;
-`
-
-export const CloseButton = styled.button`
-    border: none;
-    cursor: pointer;
-    border-radius: 8px;
-    background-color: rgb(250, 177, 160);
-    color: rgb(214, 48, 49);
-    height: 40px;
-    width: 100px;
-`
-
-export const CheckButton = styled.button`
-    border: none;
-    cursor: pointer;
-    border-radius: 8px;
-    background-color: rgb(85, 239, 196);
-    color: rgb(0, 0, 0);
-    height: 40px;
-    width: 100px;
-`
+  position: absolute;
+  bottom: 12px;
+  right: 12px;
+  display: flex;
+  gap: 5px;
+`;
