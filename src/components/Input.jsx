c30cf9901f30a4968dcc75e66e476d349ab75ac4
Sujin Lee <soolovepat@naver.com>
2023-07-07 14:22:54 +0900

    Add input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { StForm } from "../styles/InputStyle";
+import { StButton } from "../styles/ButtonStyle";
 
 const Input = () => {
+  const [product, setProduct] = useState({
+    name: "",
+    price: "",
+  });
+
+  const onChangeHandler = (event, label) => {
+    let value = event.target.value;
+
+    // 숫자만 허용하도록 정규식 검사
+    if (label === "price") {
+      value = value.replace(/[^0-9]/g, "");
+    }
+
+    // 콤마 추가
+    if (label === "price" && value.length > 3) {
+      value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    setProduct({
+      ...product,
+      [label]: value,
+    });
+  };
+
   return (
-    <div>
+    <StForm>
       <h1>Input</h1>
-    </div>
+
+      <StButton backgroundColor="#55efc4">
+        <label>이름</label>
+        <input
+          value={product.name}
+          onChange={(event) => onChangeHandler(event, "name")}
+        />
+        <label>가격</label>
+        <input
+          value={product.price}
+          onChange={(event) => onChangeHandler(event, "price")}
+        />
+        <button
+          className="small-button"
+          type="button"
+          onClick={() =>
+            alert(
+              `{ name : ${product.name}, price : ${product.price.replace(
+                /,/g,
+                ""
+              )} }`
+            )
+          }
+        >
+          저장
+        </button>
+      </StButton>
+    </StForm>
   );
 };
 
---
+++ src/styles/InputStyle.jsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const StForm = styled.form`
+  input {
+    border: 1px solid #2c2c2c;
+    line-height: 40px;
+    width: 200px;
+    outline: none;
+    border-radius: 8px;
+    padding-left: 12px;
+    padding-right: 12px;
+  }
+`;
